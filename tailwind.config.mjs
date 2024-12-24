31dbe44b0e9e44fcc6b5f7c36f27cca46295e196
Omer Miloh <omerrob11@gmail.com>
2024-12-24 13:25:56 +0200

    tailwind css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,6 +7,24 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    // colors: {
+    //   primary: "#ff0000",
+    //   grey: {
+    //     100: "#f5f5f5",
+    //     200: "#e5e5e5",
+    //     300: "#d4d4d4",
+    //     400: "#a3a3a3",
+    //     500: "#737373",
+    //     600: "#525252",
+    //     700: "#404040",
+    //     800: "#262626",
+    //     900: "#171717",
+    //   },
+    // },
+    // spacing: {},
+    // fontSize: {},
+    // fontFamily: {},
+    // fontWeight: {},
     extend: {
       colors: {
         background: "var(--background)",
