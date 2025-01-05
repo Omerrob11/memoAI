219fbd5dfba129aa666907ea9a3cc7346721ecc3
Omer Miloh <omerrob11@gmail.com>
2025-01-05 13:59:15 +0200

    feat: make api call to get get the data once we upload the file to supabase storage

--- src/servcies/supabase/storage.js
+++ src/servcies/supabase/storage.js
@@ -13,7 +13,31 @@ export async function uploadDocument(file) {
     //   we will always get data and error - one of them will be null and the other not
     // but only one has actualy value - the other one will be null
     if (error) throw error;
-    return data;
+
+    // after sucsusfull upload, we process the documenht that we just uploaded.
+    // we send in the body the document path
+    const processResponse = await fetch("/api/process-document", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // we send data over the interenet, needs to be text format
+      // we translte the js data instaed of using javascript object directly
+      // so we turn the object we have to string like
+      body: JSON.stringify({
+        documentPath: fileName,
+      }),
+    });
+
+    const processResult = await processResponse.json();
+
+    // data from supabase uploads: full path where the file is stored, file size, content type, time stamp, etc...
+    return {
+      uploadData: data,
+      // the value - it will look for it in the current scope, not in the variable name
+      // even if the variable name and the property name the same - we look for the correct variable in scope
+      processResult: processResult,
+    };
   } catch (error) {
     return { error };
     // so we could use result.error
